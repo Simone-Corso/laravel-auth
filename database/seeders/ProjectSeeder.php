7a638d4e3b4e63f20c4857b3686f01b690f62a94
Simone-Corso <scorso26@gmail.com>
2024-02-14 15:14:27 +0100

    corretto il file che non prendeva nel data

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class Project extends Seeder
+class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
